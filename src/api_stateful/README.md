39ec4b77732146324e981c777b478b92880efac7
skullquake <ockert8080@gmail.com>
2021-07-03 09:01:16 -0700

    .

---
+++ src/api_stateful/README.md
@@ -0,0 +1,3 @@
+Demonstration of creating a basic statefule API using the KWE server. Based on <a href="https://github.com/evocert/kweexamples/tree/main/src/api_basic" target="_blank">Basic API</a>
+
+The relevant file to look at is `./api/cmd/acc.js`, which makes use of `caching` to set up a key in the cache based on the module name and implements a simple accumulator.
---
+++ src/api_stateful/api/index.js
@@ -0,0 +1,54 @@
+<@
+	//--------------------------------------------------------------------------------
+	//internal api cmds here
+	//--------------------------------------------------------------------------------
+	var cmd={
+	}
+	//--------------------------------------------------------------------------------
+	var parameters={}
+	request.Parameters().StandardKeys().forEach(function(k){
+		try{
+			parameters[k.toLowerCase()]=eval(request.Parameters().StringParameter(k));
+		}catch(e){
+			parameters[k.toLowerCase()]=request.Parameters().StringParameter(k);
+		}
+	});
+	var options={};
+	Object.keys(parameters).filter(function(k,v){
+		return k!="cmd";
+	}).forEach(function(k){
+		options[k]=parameters[k];
+	});
+	try{
+		if(typeof(parameters.cmd)=="undefined"||parameters.cmd==null){
+			throw("ECMD");
+		}
+		if(typeof(cmd[parameters.cmd])=="function"){
+			try{
+
+				cmd[parameters.cmd](options);
+			}catch(e){
+				request.ResponseHeader().Set("Content-Type","application/json");
+				print(JSON.stringify({"error":e.toString()}));
+			}
+		}else{
+			//--------------------------------------------------------------------------------
+			//api cmds from ./cmd
+			//--------------------------------------------------------------------------------
+			require(["./cmd/"+parameters.cmd],function(cb){
+				if(typeof(cb)=="function"){
+					cb(options);
+				}else{
+					request.ResponseHeader().Set("Content-Type","application/json");
+					print(JSON.stringify({"error":"EMOD"}));
+				}
+			},function(e){
+				request.ResponseHeader().Set("Content-Type","application/json");
+				print(JSON.stringify({"error":e.toString()}));
+			});
+		}
+	}catch(e){
+		request.ResponseHeader().Set("Content-Type","application/json");
+		print(JSON.stringify({"error":e.toString()}));
+	}
+@>
