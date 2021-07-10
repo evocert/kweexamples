376c4a492366adfdac8594870cdd3e319d3bdf65
Ev0cert3v0v3rt <evocert@gmail.com>
2021-07-10 00:15:36 -0700

    .

--- src/webcrawler/test.sh
+++ src/webcrawler/test.sh
@@ -16,6 +16,6 @@ WGET_RECURSIVE=true
 WGET_OUTPUT=-
 WGET_MAX_VISIT=1024
 WGET_MAX_DEPTH=4
-echo curl -X GET "$URL?cmd=wget&url="$WGET_TGT"&recursive="$WGET_RECURSIVE"&debug="$WGET_DEBUG"&maxvisit="$WGET_MAX_VISIT"&maxdepth="$WGET_MAX_DEPTH"&output="$WGET_OUTPUT"" --output -
+curl -X GET "$URL?cmd=wget&url="$WGET_TGT"&recursive="$WGET_RECURSIVE"&debug="$WGET_DEBUG"&maxvisit="$WGET_MAX_VISIT"&maxdepth="$WGET_MAX_DEPTH"&output="$WGET_OUTPUT"" --output -
 #curl -X POST -H "Content-Type: application/json" --data '{"cmd":"wget","url":"'$WGET_TGT'","recursive":'$WGET_RECURSIVE',"maxdepth":'$WGET_MAX_DEPTH',"maxvisit":'$WGET_MAX_VISIT',"output":"'$WGET_OUTPUT'","debug":'$WGET_DEBUG'}' "$URL" --output -
 #curl -X POST -H "Content-Type: application/xml" --data '<?xml version="1.0" encoding="UTF-8"?><api><cmd>wget</cmd><url>'$WGET_TGT'</url><recursive>'$WGET_RECURSIVE'</recursive><maxvisit>'$WGET_MAX_VISIT'</maxvisit><maxdepth>'$WGET_MAX_DEPTH'</maxdepth><debug>'$WGET_DEBUG'</debug><output>'$WGET_OUTPUT'</output></api>' "$URL" --output -
