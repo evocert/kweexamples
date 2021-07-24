58f2f4f3a4678490839db1e2c8899f358d2e0a45
Ev0cert3v0v3rt <evocert@gmail.com>
2021-07-24 11:52:50 +0200

    .

--- src/graphing/res/test0.js
+++ src/graphing/res/test0.js
@@ -2,8 +2,10 @@
 	try{
 		var sf=new StorageFactory();
 		var s=sf.create();
-		s.init("a",0);
+		//s.clear();
+		s.init("a",Math.random());
 		s.init("b",[1,2,3,4]);
+		s.set("c",Math.random());
 		return s.toJson();
 	}catch(e){
 		return e.toString()
